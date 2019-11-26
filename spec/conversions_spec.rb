b4d198585ff14d5c4876b5225fda367a0b22359a
alice T <alicewtien@gmail.com>
2019-11-26 16:59:49 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -1,13 +1,13 @@
 require_relative '../conversions.rb'
 
-describe "conversions" do
-  describe '#ounces_to_grams' do
-    it 'given zero, returns 0.0' do
-      grams = ounces_to_grams(0)
-      expect(grams).to eq(0.0)
-    end
-  end
-end
+#describe "conversions" do
+#  describe '#ounces_to_grams' do
+#    it 'given zero, returns 0.0' do
+#      grams = ounces_to_grams(0)
+#      expect(grams).to eq(0.0)
+#    end
+#  end
+#end
 
 describe "conversions" do
   describe '#cups_to_mls' do
