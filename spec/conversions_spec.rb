6aecd79aa3f16bbe067afd478add3b1c7f934aaa
alice T <alicewtien@gmail.com>
2019-11-26 16:59:36 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -8,3 +8,12 @@ describe "conversions" do
     end
   end
 end
+
+describe "conversions" do
+  describe '#cups_to_mls' do
+    it 'given zero, returns 0.0' do
+      mls = cups_to_mls(0)
+      expect(mls).to eq(0.0)
+    end
+  end
+end
